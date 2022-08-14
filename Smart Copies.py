44432177176a7b184cc4dee59361b7f87a87cb57
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2022-08-14 17:22:48 -0500

    Completed the SmartCopies addin alongside its resources.

--- Smart Copies.py
+++ Smart Copies.py
@@ -1,8 +1,9 @@
-#Author-
-#Description-
-
+# Author- Juan Gras
+from . import commands
 import adsk.core, adsk.fusion, adsk.cam, traceback
 import ctypes
+from .lib import fusion360utils as futil
+handlers=[]
 
 CF_TEXT = 1
 kernel32 = ctypes.windll.kernel32
@@ -25,69 +26,110 @@ def get_clipboard_text():
     finally:
         user32.CloseClipboard()
 
-def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
+class SC_CreateButtonPressedEventHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self,args):
+        app=adsk.core.Application.get()
+        ui=app.userInterface
         design = app.activeProduct
 
-        #Get dataprojects collection
         dataProject = app.data.dataProjects[0]
-        #Gets rootFolder from the collection
         rootFolder = dataProject.rootFolder
         doc = app.activeDocument
-
-        #Gets the user parameters collection
         uParams = design.userParameters
-        #uParams.itemByName("height").expression = "4mm"
 
-
-        #Gets excel table copied on clipboard
+        # Gets excel table copied on clipboard
         excel_clipboard = get_clipboard_text()
-        #Decodes excel table and converts it to string
+        # Decodes excel table and converts it to string
         excel_clipboard = excel_clipboard.decode('UTF-8')
         excel_clipboard = str(excel_clipboard)
-        #Removes row delimiter \r and splits the table rows into lists
+        # Removes row delimiter \r and splits the table rows into lists
         excel_clipboard = excel_clipboard.replace("\r","")
         excel_clipboard = excel_clipboard.split("\n")
-        #Each column will be separated by special delimiter \t, so it will be used to split into individual words
+        # Each column will be separated by special delimiter \t, so it will be used to split into individual words
         for i in range(0,len(excel_clipboard)-1):
             excel_clipboard[i] = excel_clipboard[i].split("\t")
-        #print(excel_clipboard)
 
-        #Sets column headers from the first list of excel_clipboard and removes it to avoid conflict
+        # Sets column headers from the first list of excel_clipboard and removes it to avoid conflict
         columnHeaders = excel_clipboard[0]
         del excel_clipboard[0]
         file_names = ""
-        #Iterates through data rows only
+        # Iterates through data rows only
         for i in range(len(excel_clipboard)-1):
             row = excel_clipboard[i]
             nameoffile = "SmartCopy_" + str(i+1)
-            #Iterates through each column of the row
+            # Iterates through each column of the row
             for j in range(len(row)):
                 word = row[j]        
                 colheader = columnHeaders[j]
-                #Checks if the column header refers to the file's name instead of a user parameter and sets it if that's the case
+                # Checks if the column header refers to the file's name instead of a user parameter and sets it if that's the case
                 if colheader.replace(" ","").replace("_","").lower() == "nameoffile":
                     nameoffile = row[j]
                 else:
                     paramvalue = row[j]
                     paramname = colheader
                     try:
-                        #Update the user parameter by expression to avoid issues with units
+                        # Update the user parameter by expression to avoid issues with units
                         uParams.itemByName(paramname).expression = paramvalue
                     except:
-                        #Exception so execution doesn't stop on mistake (for better or worse)
+                        # Exception so execution doesn't stop on mistake (for better or worse)
                         ui.messageBox("Error while trying to update user parameter '", paramname, "' with expresion '", paramvalue, "'")
             try:
                 doc.saveAs(nameoffile, rootFolder, '', '')
             except:
                 ui.messageBox("Error while trying to save file '", nameoffile, "' under folder '", dataProject.name)
-            #Appends all the file names into a single message
+            # Appends all the file names into a single message
             file_names = file_names + nameoffile + ", "
         msg = f'All documents were saved to\n project: "{dataProject.name}" with the file names: "{file_names}"'
         ui.messageBox(msg)
+
+def run(context):
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        cmdDefs = ui.commandDefinitions
+        workSpace = ui.workspaces.itemById('FusionSolidEnvironment')
+        tbPanels = workSpace.toolbarPanels
+        
+        #region Delete previous run created button and panel
+        global tbPanel
+        tbPanel = tbPanels.itemById('SmartCopiesPanel')
+        if tbPanel:
+            tbPanel.deleteMe()
+        global SC_CreateButton
+        SC_CreateButton = ui.commandDefinitions.itemById('SC_CreateButton')
+        if SC_CreateButton:
+            SC_CreateButton.deleteMe()
+        #endregion
+
+        # Create a toolbar panel next to the Select Panel
+        tbPanel = tbPanels.add('SmartCopiesPanel', 'Smart Copies', 'SelectPanel', False)
+        # Create a button command definition.
+        SC_CreateButton = cmdDefs.addButtonDefinition('SC_CreateButton', 'Create Copies', 'Creates copies of current file using excel data stored on clipboard','Resources/Printer')
+        # Create the button control
+        buttonControl = tbPanel.controls.addCommand(SC_CreateButton)
+
+        # Event Handlers
+        SC_CreateButtonPressed=SC_CreateButtonPressedEventHandler()
+        SC_CreateButton.commandCreated.add(SC_CreateButtonPressed)
+        handlers.append(SC_CreateButtonPressed)
+        
+        # Make the button available in the panel at the top.
+        buttonControl.isPromotedByDefault = True
+        buttonControl.isPromoted = True
     except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+        futil.handle_error('run')
+
+def stop(context):
+    try:
+        # Remove all of the event handlers
+        futil.clear_handlers()
+        # Remove the toolbar panel and its button
+        if tbPanel:
+            tbPanel.deleteMe()
+        if SC_CreateButton:
+            SC_CreateButton.deleteMe()
+        commands.stop()
+    except:
+        futil.handle_error('stop')
